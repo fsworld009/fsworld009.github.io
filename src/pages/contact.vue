7536d1425288bfa0c5442c536761f074b932d243
WorldFS <fsworld009@gmail.com>
2021-10-12 22:06:21 +0800

    contact

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,3 +1,44 @@
 <template>
-  <div>Contact</div>
+  <div class="contact">
+    <div class="contact__links">
+      <a class="contact__link" href="mailto:fsworld009@gmail.com">
+        <FaIcon icon="envelope" /> Email
+      </a>
+      <a class="contact__link" href="https://github.com/fsworld009" target="_blank">
+        <FaIcon icon="github" /> GitHub
+      </a>
+      <a class="contact__link" href="https://twitter.com/fsworld009" target="_blank">
+        <FaIcon icon="twitter" /> Twitter
+      </a>
+    </div>
+    <Back class="contact__back" />
+  </div>
 </template>
+
+<style lang="less">
+@import '~/assets/variables.less';
+
+.contact {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+}
+
+.contact__links {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+}
+
+.contact__link {
+  margin-top: 12px;
+  font-size: @text_size_normal;
+  width: @text_size_normal * 11;
+}
+
+.contact__back {
+  margin-top: 24;
+}
+</style>
